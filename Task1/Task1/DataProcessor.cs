ab9c796865462bece5c129ad188b47f312d4d4db
Anastasia <nastya.kaneva.99@gmail.com>
2026-05-24 20:38:30 +0300

    Task1

---
+++ Task1/Task1/DataProcessor.cs
@@ -0,0 +1,70 @@
+﻿namespace Task1
+{
+    internal class DataProcessor
+    {
+
+        public decimal[]? ProcessDataSequential(decimal[] data)
+        {
+            if (data == null || data.Length == 0)
+            {
+                return null;
+            }
+
+            decimal[] result = new decimal[data.Length];
+
+            Array.Copy(data, result, data.Length);
+
+            this.RealProcessData(ref result);
+
+            return result;
+        }
+
+        public decimal[]? ProcessDataParallel(decimal[] data, int threadCount)
+        {
+            if (data == null || data.Length == 0 || threadCount <= 0)
+            {
+                return null;
+            }
+
+            int arrayLenForThread = data.Length / threadCount;
+            decimal[] resultArray = new decimal[data.Length];
+
+            List<Thread> threads = new List<Thread>();
+
+            for (int i = 0; i < threadCount; ++i)
+            {
+                decimal[] threadArray = new decimal[arrayLenForThread];
+
+                int resultArrayIndex = i * arrayLenForThread;
+
+                Array.Copy(data, resultArrayIndex, threadArray, 0, arrayLenForThread);
+
+                Thread thread = new Thread(() =>
+                {
+                    this.RealProcessData(ref threadArray);
+                    Array.Copy(threadArray, 0, resultArray, resultArrayIndex, arrayLenForThread);
+                });
+
+                threads.Add(thread);
+                thread.Start();
+            }
+
+            foreach (var t in threads)
+            {
+                t.Join();
+            }
+
+            return resultArray;
+        }
+
+        private void RealProcessData(ref decimal[] result)
+        {
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = (decimal)Math.Sqrt((double)result[i]) * (decimal)Math.Log10((double)result[i] + 1);
+            }
+        }
+    }
+
+
+}
---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿using Task1;
+
+int seed = 42;
+int arraySize = 10_000_000;
+int threadsCount = 4;
+
+decimal[] originalData = new decimal[arraySize];
+
+Random rand = new Random(seed);
+Task1.DataProcessor processor = new Task1.DataProcessor();
+
+
+for (int i = 0; i < originalData.Length; i++)
+{
+    originalData[i] = (decimal) (1.0 + rand.NextDouble() * 999.0);
+}
+
+decimal[]? ProcessSequential()
+{
+    return processor.ProcessDataSequential(originalData);
+}
+
+decimal[]? ProcessParallel()
+{
+    return processor.ProcessDataParallel(originalData, threadsCount);
+}
+
+(TimeSpan seqTime, decimal[]? seqRes) = PerformanceMeter.MeasureExecutionTime(ProcessSequential, "Последовательная обработка");
+(TimeSpan parallelTime, decimal[]? parallelRes) = PerformanceMeter.MeasureExecutionTime(ProcessParallel, "Параллельная обработка");
+
+bool isEquals = Task1.PerformanceMeter.CompareResults(seqRes, parallelRes);
+
+Console.WriteLine("=== Результаты обработки ===");
+Console.WriteLine($"Размер данных: {arraySize} элементов");
+Console.WriteLine($"Последовательная обработка: {seqTime.TotalMilliseconds} мс");
+Console.WriteLine($"Параллельная обработка ({threadsCount} потока): {parallelTime.TotalMilliseconds} мс");
+Console.WriteLine($"Ускорение: {seqTime.TotalMilliseconds / parallelTime.TotalMilliseconds} x");
+Console.WriteLine($"Результаты совпадают: {(isEquals ? "Да" : "Нет")}");
